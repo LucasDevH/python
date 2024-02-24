653c15618ccbcb0ca24e223a7a77104b83de7f5c
Lucas H Oliveira <lucas.dev.henrique@gmail.com>
2024-02-24 14:58:42 -0300

    tesste
    teste

---
+++ nome.py
@@ -0,0 +1,4 @@
+data = input('Digite a data de nascimento: ')
+mes = input('Digite o mes de nascimento: ')
+ano = input('Digite o ano de nascimento: ')
+print ('Voce nasceu no dia ', data, 'do mes', mes, 'no ano', ano)
